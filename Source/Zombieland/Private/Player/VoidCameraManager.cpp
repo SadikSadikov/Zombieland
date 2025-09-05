e2e9acbbea3b96aee14742b1f36fe8f4456d700f
Sadak Sadakov <sadikscorpion@gmail.com>
2025-09-05 12:33:47 +0300

    Camera Manager

---
+++ Source/Zombieland/Private/Player/VoidCameraManager.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/VoidCameraManager.h"
+#include "Kismet/KismetMathLibrary.h"
+
+AVoidCameraManager::AVoidCameraManager()
+{
+	bIsOrthographic = true;
+	DefaultOrthoWidth = 3000.f;
+}
+
+void AVoidCameraManager::UpdateViewTarget(FTViewTarget& OutVT, float DeltaTime)
+{
+	FVector ActorLocation = OutVT.Target->GetActorLocation();
+
+	FVector CalcLocation = ActorLocation + CameraLocOffset;
+	OutVT.POV.Location = CalcLocation;
+	OutVT.POV.Rotation = UKismetMathLibrary::FindLookAtRotation(CalcLocation, ActorLocation);
+}
---
+++ Source/Zombieland/Public/Player/VoidCameraManager.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/PlayerCameraManager.h"
+#include "VoidCameraManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZOMBIELAND_API AVoidCameraManager : public APlayerCameraManager
+{
+	GENERATED_BODY()
+
+public:
+	
+	AVoidCameraManager();
+
+protected:
+
+	virtual void UpdateViewTarget(FTViewTarget& OutVT, float DeltaTime) override;
+
+private:
+
+	UPROPERTY(EditDefaultsOnly, Category = "Camera Properties")
+	FVector CameraLocOffset;
+	
+};
