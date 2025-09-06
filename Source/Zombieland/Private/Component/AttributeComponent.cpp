06741849db6e67f5209507b809ca83ca5235d19f
Sadak Sadakov <sadikscorpion@gmail.com>
2025-09-06 10:50:26 +0300

    Attribute Component

---
+++ Source/Zombieland/Private/Component/AttributeComponent.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Component/AttributeComponent.h"
+
+UAttributeComponent::UAttributeComponent()
+{
+	
+	PrimaryComponentTick.bCanEverTick = false;
+	
+}
+
+
+void UAttributeComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	
+}
+
